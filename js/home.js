c958f56b0b011f796ef587a27e5ec89cd332f8a3
charade <charles.ekomie@gmail.com>
2021-02-11 17:35:40 +0100

    refectored home page design

--- js/home.js
+++ js/home.js
@@ -1,3 +1,20 @@
+/***********STICKY EFFECT ON THE NAVBAR*********/
+let nav = document.querySelector('nav');
+let upperNav = document.querySelector('.media');
+// let upperNavHeight = window.getComputedStyle(upperNav).getPropertyValue('height');
+window.addEventListener('scroll',stick);
+
+function stick(){
+    if(window.scrollY > upperNav.offsetTop){
+        document.body.classList.add('stick-the-nav');
+    }
+    else{
+        document.body.classList.remove('stick-the-nav');
+    }
+}
+
+
+
 /*******SWITCHING THEME MODE NAVBAR BUTTON****** */
 let slider = document.querySelector('.slider')
 let wraper = document.querySelector(".slider-canvas")
