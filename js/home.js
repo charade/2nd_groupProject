587943b4ca96ac03dce63ecff8613ee8b7344ff4
charade <charles.ekomie@gmail.com>
2021-02-12 21:10:22 +0100

    new home.html

--- js/home.js
+++ js/home.js
@@ -1,3 +1,14 @@
+/*************************************** */
+let article = document.createElement('div');
+let title = document.createElement('h3');
+article.appendChild(title);
+article.classList.add('article');
+title.classList.add('article-title')
+let article_container = document.querySelector('.articles-container');
+article_container.appendChild(article)
+title.textContent = "title"
+/************************************** */
+
 /***********STICKY EFFECT ON THE NAVBAR*********/
 let nav = document.querySelector('nav');
 let upperNav = document.querySelector('.media');
@@ -13,9 +24,7 @@ function stick(){
     }
 }
 
-
-
-/*******SWITCHING THEME MODE NAVBAR BUTTON****** */
+/*******SWITCHING THEME MODE NAVBAR BUTTON*******/
 let slider = document.querySelector('.slider')
 let wraper = document.querySelector(".slider-canvas")
 
@@ -24,6 +33,45 @@ let clickable = wraper || slider
 clickable.addEventListener('click',darkMode);
 
 function darkMode(){
-    console.log('coucou')
     document.body.classList.toggle('darkedTheme')
 }
+
+
+/*********************************************************/
+/********************* Pop up dynamic creation *******/
+
+/* 1 ************* Pop up window **********************/
+
+//block juste en dessous de la nav Bar
+let mainContainer = document.querySelector('.main-container')
+//crétion dynamique de notre fenêtre pop up
+let popWindow = document.createElement('div')
+document.body.appendChild(popWindow)
+
+let template = document.querySelector('.article')
+template.addEventListener('click',postInFront)
+
+function postInFront(){
+    popWindow.classList.add('pop-up-window');
+    mainContainer.classList.add('pop-up');
+}
+
+/*********** 2 pop up window closing button ***********/
+
+//button creation and posted on the pop up window
+let close_pop_up = document.createElement('img');
+close_pop_up.src = "../asset/cancel.svg";
+popWindow.appendChild(close_pop_up);
+close_pop_up.classList.add('close');
+
+// Event to close the pop window
+let closing_area =  close_pop_up || mainContainer;
+closing_area.addEventListener('click',closePopUp)
+//call back to close the pop up window
+function closePopUp(){
+    console.log('coucou')
+    popWindow.classList.remove('pop-up-window');
+    mainContainer.classList.remove('pop-up');
+}
+
+
